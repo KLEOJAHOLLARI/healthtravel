4e95aab64c9bf293e8f4b01e3dad0492abc3b8f3
kleo jahollari <kleojahollai43@gmail.com>
2026-05-18 21:08:47 +0200

    header sticky changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,24 @@
-/* YEAR */
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+
+const headerEl = document.querySelector(".header");
+
+//fix header
+if (btnNavEl) {
+  btnNavEl.addEventListener("click", function () {
+    headerEl.classList.toggle("nav-open");
+  });
+}
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 50) {
+    headerEl.classList.add("sticky");
+  } else {
+    headerEl.classList.remove("sticky");
+  }
+});
+
 const year = document.getElementById("year");
 
 if (year) {
   year.textContent = new Date().getFullYear();
 }
-
-/* STICKY HEADER */
