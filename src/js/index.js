88815b6f638e7e4bf9dcc92ddf21916d9af382e6
kleo jahollari <kleojahollai43@gmail.com>
2026-05-20 20:09:42 +0200

    eye page improvment

--- src/js/index.js
+++ src/js/index.js
@@ -64,26 +64,28 @@ window.addEventListener("scroll", function () {
 });
 
 //counter
-const counters = document.querySelectorAll(".counter");
+document.addEventListener("DOMContentLoaded", function () {
+  const counters = document.querySelectorAll(".counter");
 
-const speed = 200;
+  const speed = 200;
 
-counters.forEach((counter) => {
-  const animate = () => {
-    const value = +counter.getAttribute("data-count");
+  counters.forEach((counter) => {
+    const animate = () => {
+      const value = +counter.getAttribute("data-count");
 
-    const data = +counter.innerText;
+      const data = +counter.innerText;
 
-    const time = value / speed;
+      const time = value / speed;
 
-    if (data < value) {
-      counter.innerText = Math.ceil(data + time);
+      if (data < value) {
+        counter.innerText = Math.ceil(data + time);
 
-      setTimeout(animate, 15);
-    } else {
-      counter.innerText = value;
-    }
-  };
+        setTimeout(animate, 15);
+      } else {
+        counter.innerText = value;
+      }
+    };
 
-  animate();
+    animate();
+  });
 });
